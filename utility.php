c96030fc2c7071584da8fb99e6db9c846e31d64d
crossgate9 <crossgate9@gmail.com>
2013-12-09 17:51:37 +0800

    [bug] product status condition error

--- utility.php
+++ utility.php
@@ -17,6 +17,14 @@ function getParentProduct($_product) {
     }
 }
 
+function isProductVisible($_product) {
+    return !((int) $_product->getData('visibility') === Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE);
+}
+
+function isProductEnabled($_product) {
+    return !((int) $_product->getData('status') === Mage_Catalog_Model_Product_Status::STATUS_DISABLED);
+}
+
 function isProductInvalid($_product) {
     $_data = $_product->getData();
     return ((int) $_data['visibility'] === Mage_Catalog_Model_Product_Visibility::VISIBILITY_NOT_VISIBLE)
@@ -40,11 +48,17 @@ function getRecords($_store_id) {
     	if (isset($_parent_product_data)) unset($_parent_product_data);
 
     	$_product = Mage::getModel('catalog/product')->load($_product->getId());
-        if (isProductInvalid($_product) === false) continue;
-
+        if (isProductEnabled($_product) === false) continue;
 
         $_parent_product = getParentProduct($_product);
-        if ($_parent_product !== false && isProductInvalid($_parent_product) === false) continue;
+        if ($_parent_product !== false && 
+           (isProductEnabled($_parent_product) === false || isProductVisible($_parent_product) == false)) {
+            continue;
+        }
+
+        if ($_parent_product === false && isProductVisible($_parent_product) === false) {
+            continue;
+        }
 
     	$_data = $_product->getData();
         $_parent_product_data = ($_parent_product === false) ? $_data : $_parent_product->getData();
@@ -63,10 +77,10 @@ function getRecords($_store_id) {
             'sku' => $_data['sku'], // xxx simple product sku
             'category' => '', // xxx category
             'size' => '', // xxx simple product size
-    	    'brand' => $_product->getAttributeText('brand'),
+    	    // 'brand' => $_product->getAttributeText('brand'),
     	    'stock' => ($_data['is_in_stock']) ? '1-2 werkdagen' : '3-5 werkdagen',
             'varient' => $_parent_product_data['sku'],
-    	    'size' => ($_parent_product !== false) ? $_product->getAttributeText('size') : 0
+    	    // 'size' => ($_parent_product !== false) ? $_product->getAttributeText('size') : 0
         );
 
     	$_records[] = $_record;
