a87bad8252005f9b51aa098ad8c98a0e723326ae
Allen Koo <crossgate9@gmail.com>
2014-10-16 17:42:23 +0800

    [add] prefix and suffix for product name

--- lib/Crossgate9/Product/Name.php
+++ lib/Crossgate9/Product/Name.php
@@ -7,7 +7,11 @@ class Crossgate9_Product_Name extends Crossgate9_Product_Abstract {
 
   public function getValue($_options = array()) {
     $_product = $this->_entities['product'];
-    $this->_value = $_product->getData('name');
+
+    $_prefix = isset($_options['prefix']) ? $_options['prefix'] : '';
+    $_suffix = isset($_options['suffix']) ? $_options['suffix'] : '';
+
+    $this->_value = $_prefix . $_product->getData('name') . $_suffix;
     return $this->_value;
   }
 }
